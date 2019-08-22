ebbc9497b0207d3c460bb0c4289fb284b4414521
GabrielleRab <32782626+GabrielleRab@users.noreply.github.com>
2019-08-22 15:18:07 -0400

    Update bridgeup.py

--- bridgeup.py
+++ bridgeup.py
@@ -2,7 +2,3 @@
 
 # Add a comment at the bottom of the file explaining what your function does 
 # and then add your function below
-
-# This is a test function
-def testFunc(testnum):
-	return "the test number is " + str(testnum)
