59b66f9e93a648df38d4bc1ccd182d21b90b3cd6
GabrielleRab <32782626+GabrielleRab@users.noreply.github.com>
2019-08-22 15:26:06 -0400

    Update bridgeup.py

--- bridgeup.py
+++ bridgeup.py
@@ -2,3 +2,7 @@
 
 # Add a comment at the bottom of the file explaining what your function does 
 # and then add your function below
+
+import pandas as pd
+
+df = pd.read_csv("internship_bootcamp_data.csv")
